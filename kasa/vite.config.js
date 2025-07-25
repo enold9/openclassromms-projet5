85be53a1c53a66e29c864474139886b20675df00
enold9 <nold.eloic@gmail.com>
2025-07-25 20:05:53 +0200

    build

--- kasa/vite.config.js
+++ kasa/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'openclassromms-projet5'
+  base: 'openclassromms-projet5/kasa'
 })
